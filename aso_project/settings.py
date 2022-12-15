ea6431583ef5cf2bbe85fe74d699443dafc2648e
Dragos Marian Stahie <dragosstahie@gmail.com>
2022-12-15 10:50:14 +0200

    Added connection to mysql

--- aso_project/settings.py
+++ aso_project/settings.py
@@ -88,10 +88,15 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'asodb',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
