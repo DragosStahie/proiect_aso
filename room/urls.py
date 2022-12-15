8909839c87940c307ea0afc74d3a1b11512db457
Dragos Marian Stahie <dragosstahie@gmail.com>
2022-12-15 09:04:33 +0200

    Added option of creating new rooms

--- room/urls.py
+++ room/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.rooms, name='rooms'),
+    path('new/<slug:slug>/', views.new_room, name='new_room'),
     path('<slug:slug>/', views.room, name='room'),
 ]
--- room/views.py
+++ room/views.py
@@ -12,6 +12,14 @@ def rooms(request):
 
 
 @login_required
+def new_room(request, slug):
+    if len(Room.objects.filter(name = slug)) == 0:
+        Room.objects.create(name=slug, slug=slug)
+
+    return rooms(request)
+
+
+@login_required
 def room(request, slug):
     room = Room.objects.get(slug=slug)
     messages = Message.objects.filter(room=room)[0:25]
